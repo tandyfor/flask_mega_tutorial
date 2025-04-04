c5779c03a91ee0d7bfa355b94338e95a7f184bf0
tandyfor <alex.mitruagin@gmail.com>
2025-04-04 18:51:01 +0300

    Chapter 2.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, Flask!'
+    user = {'username' : 'Aleksey'}
+    posts = [
+        {
+            'author' : {'username' : 'Vladislav'},
+            'body' : 'New Features MacOS.'
+        },
+        {
+            'author' : {'username' : 'Aleksander'},
+            'body' : 'POSIX Std.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
