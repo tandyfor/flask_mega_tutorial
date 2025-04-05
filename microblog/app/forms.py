6599fa60863ddf5a075e6e835abdf59380314450
tandyfor <alex.mitruagin@gmail.com>
2025-04-05 20:28:45 +0300

    Chapter 5.

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
+
+import sqlalchemy as sa
+
+from app import db
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +13,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sing In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Usernmae', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username==username.data))
+        if user is not None:
+            raise ValueError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email==email.data))
+        if user is not None:
+            raise ValueError('Please use a different email address.')
